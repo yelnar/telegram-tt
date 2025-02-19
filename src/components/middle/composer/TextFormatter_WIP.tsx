bfd466669bc4cafbeda0f8de6e6c6d07782d0040
Yelnar <yelnar.nauryzbayev@gmail.com>
2025-02-19 12:18:47 +0400

    wip - rich text editor

---
+++ src/components/middle/composer/TextFormatter_WIP.tsx
@@ -0,0 +1,377 @@
+import type { FC } from '../../../lib/teact/teact';
+import React, {
+  memo, useEffect, useRef, useState,
+} from '../../../lib/teact/teact';
+
+import type { IAnchorPosition } from '../../../types';
+import { ApiMessageEntityTypes } from '../../../api/types';
+
+import { EDITABLE_INPUT_ID } from '../../../config';
+import buildClassName from '../../../util/buildClassName';
+import captureEscKeyListener from '../../../util/captureEscKeyListener';
+import {
+  insertLink,
+  toggleMonospace,
+  toggleQuote,
+  toggleSpoiler,
+} from '../../../util/dom/dom';
+import { ensureProtocol } from '../../../util/ensureProtocol';
+import getKeyFromEvent from '../../../util/getKeyFromEvent';
+import stopEvent from '../../../util/stopEvent';
+
+import useFlag from '../../../hooks/useFlag';
+import useLastCallback from '../../../hooks/useLastCallback';
+import useOldLang from '../../../hooks/useOldLang';
+import useShowTransitionDeprecated from '../../../hooks/useShowTransitionDeprecated';
+import useVirtualBackdrop from '../../../hooks/useVirtualBackdrop';
+
+import Icon from '../../common/icons/Icon';
+import Button from '../../ui/Button';
+
+import './TextFormatter.scss';
+
+export type OwnProps = {
+  isOpen: boolean;
+  anchorPosition?: IAnchorPosition;
+  selectedRange?: Range;
+  // setSelectedRange: (range: Range) => void;
+  onClose: () => void;
+  onToggleBold: () => void;
+  onToggleItalic: () => void;
+  onToggleUnderline: () => void;
+  onToggleStrikethrough: () => void;
+};
+
+interface ISelectedTextFormats {
+  bold?: boolean;
+  italic?: boolean;
+  underline?: boolean;
+  strikethrough?: boolean;
+  monospace?: boolean;
+  spoiler?: boolean;
+  quote?: boolean;
+}
+
+const TEXT_FORMAT_BY_TAG_NAME: Record<string, keyof ISelectedTextFormats> = {
+  B: 'bold',
+  STRONG: 'bold',
+  I: 'italic',
+  EM: 'italic',
+  U: 'underline',
+  DEL: 'strikethrough',
+  CODE: 'monospace',
+  SPAN: 'spoiler',
+  BLOCKQUOTE: 'quote',
+};
+
+const TextFormatter: FC<OwnProps> = ({
+  isOpen,
+  anchorPosition,
+  selectedRange,
+  // setSelectedRange,
+  onToggleBold,
+  onToggleItalic,
+  onToggleUnderline,
+  onToggleStrikethrough,
+  onClose,
+}) => {
+  // eslint-disable-next-line no-null/no-null
+  const containerRef = useRef<HTMLDivElement>(null);
+  // eslint-disable-next-line no-null/no-null
+  const linkUrlInputRef = useRef<HTMLInputElement>(null);
+  const { shouldRender, transitionClassNames } = useShowTransitionDeprecated(isOpen);
+  const [isLinkControlOpen, openLinkControl, closeLinkControl] = useFlag();
+  const [linkUrl, setLinkUrl] = useState('');
+  const [inputClassName, setInputClassName] = useState<string | undefined>();
+  const [selectedTextFormats, setSelectedTextFormats] = useState<ISelectedTextFormats>({});
+
+  useEffect(() => (isOpen ? captureEscKeyListener(onClose) : undefined), [isOpen, onClose]);
+  useVirtualBackdrop(
+    isOpen,
+    containerRef,
+    onClose,
+    true,
+  );
+
+  useEffect(() => {
+    if (isLinkControlOpen) {
+      linkUrlInputRef.current!.focus();
+    } else {
+      setLinkUrl('');
+    }
+  }, [isLinkControlOpen]);
+
+  useEffect(() => {
+    if (!shouldRender) {
+      closeLinkControl();
+      setSelectedTextFormats({});
+      setInputClassName(undefined);
+    }
+  }, [closeLinkControl, shouldRender]);
+
+  // useEffect(() => {
+  //   if (!isOpen || !selectedRange) {
+  //     return;
+  //   }
+
+  //   const selectedFormats: ISelectedTextFormats = {};
+  //   let node = selectedRange.commonAncestorContainer.nodeType === Node.TEXT_NODE
+  //     ? selectedRange.commonAncestorContainer.parentElement
+  //     : selectedRange.commonAncestorContainer as HTMLElement;
+  //   while (node && node.id !== EDITABLE_INPUT_ID) {
+  //     const textFormat = TEXT_FORMAT_BY_TAG_NAME[node.tagName];
+  //     if (textFormat) {
+  //       selectedFormats[textFormat] = true;
+  //     }
+
+  //     node = node.parentElement;
+  //   }
+
+  //   setSelectedTextFormats(selectedFormats);
+  // }, [isOpen, selectedRange, openLinkControl]);
+
+  function updateInputStyles() {
+    const input = linkUrlInputRef.current;
+    if (!input) {
+      return;
+    }
+
+    const { offsetWidth, scrollWidth, scrollLeft } = input;
+    if (scrollWidth <= offsetWidth) {
+      setInputClassName(undefined);
+      return;
+    }
+
+    let className = '';
+    if (scrollLeft < scrollWidth - offsetWidth) {
+      className = 'mask-right';
+    }
+    if (scrollLeft > 0) {
+      className += ' mask-left';
+    }
+
+    setInputClassName(className);
+  }
+
+  function handleLinkUrlChange(e: React.ChangeEvent<HTMLInputElement>) {
+    setLinkUrl(e.target.value);
+    updateInputStyles();
+  }
+
+  function getFormatButtonClassName(key: keyof ISelectedTextFormats) {
+    if (selectedTextFormats[key]) {
+      return 'active';
+    }
+
+    if (key === 'monospace' || key === 'strikethrough') {
+      if (Object.keys(selectedTextFormats).some(
+        (fKey) => fKey !== key && Boolean(selectedTextFormats[fKey as keyof ISelectedTextFormats]),
+      )) {
+        return 'disabled';
+      }
+    } else if (selectedTextFormats.monospace || selectedTextFormats.strikethrough) {
+      return 'disabled';
+    }
+
+    return undefined;
+  }
+
+  const handleSpoilerText = useLastCallback(() => {
+    toggleSpoiler(EDITABLE_INPUT_ID, ApiMessageEntityTypes.Spoiler);
+  });
+
+  const handleMonospaceText = useLastCallback(() => {
+    toggleMonospace(EDITABLE_INPUT_ID);
+  });
+
+  const handleQuoteText = useLastCallback(() => {
+    toggleQuote(EDITABLE_INPUT_ID);
+  });
+
+  const handleLinkUrlConfirm = useLastCallback(() => {
+    if (!selectedRange) {
+      return;
+    }
+    const formattedLinkUrl = (ensureProtocol(linkUrl) || '').split('%').map(encodeURI).join('%');
+    insertLink(EDITABLE_INPUT_ID, selectedRange, formattedLinkUrl);
+    onClose();
+  });
+
+  const handleKeyDown = useLastCallback((e: KeyboardEvent) => {
+    const HANDLERS_BY_KEY: Record<string, AnyToVoidFunction> = {
+      k: openLinkControl,
+      b: onToggleBold,
+      u: onToggleUnderline,
+      i: onToggleItalic,
+      m: handleMonospaceText,
+      s: onToggleStrikethrough,
+      p: handleSpoilerText,
+      q: handleQuoteText,
+    };
+
+    const handler = HANDLERS_BY_KEY[getKeyFromEvent(e)];
+
+    if (
+      e.altKey
+      || !(e.ctrlKey || e.metaKey)
+      || !handler
+    ) {
+      return;
+    }
+
+    e.preventDefault();
+    e.stopPropagation();
+    handler();
+  });
+
+  useEffect(() => {
+    if (isOpen) {
+      document.addEventListener('keydown', handleKeyDown);
+    }
+
+    return () => document.removeEventListener('keydown', handleKeyDown);
+  }, [isOpen, handleKeyDown]);
+
+  const lang = useOldLang();
+
+  function handleContainerKeyDown(e: React.KeyboardEvent<HTMLDivElement>) {
+    if (e.key === 'Enter' && isLinkControlOpen) {
+      handleLinkUrlConfirm();
+      e.preventDefault();
+    }
+  }
+
+  if (!shouldRender) {
+    return undefined;
+  }
+
+  const className = buildClassName(
+    'TextFormatter',
+    transitionClassNames,
+    isLinkControlOpen && 'link-control-shown',
+  );
+
+  const linkUrlConfirmClassName = buildClassName(
+    'TextFormatter-link-url-confirm',
+    Boolean(linkUrl.length) && 'shown',
+  );
+
+  const style = anchorPosition
+    ? `left: ${anchorPosition.x}px; top: ${anchorPosition.y}px;--text-formatter-left: ${anchorPosition.x}px;`
+    : '';
+
+  return (
+    <div
+      ref={containerRef}
+      className={className}
+      style={style}
+      onKeyDown={handleContainerKeyDown}
+      // Prevents focus loss when clicking on the toolbar
+      onMouseDown={stopEvent}
+    >
+      <div className="TextFormatter-buttons">
+        <Button
+          color="translucent"
+          ariaLabel="Spoiler text"
+          className={getFormatButtonClassName('spoiler')}
+          onClick={handleSpoilerText}
+        >
+          <Icon name="eye-closed" />
+        </Button>
+        <div className="TextFormatter-divider" />
+        <Button
+          color="translucent"
+          ariaLabel="Bold text"
+          className={getFormatButtonClassName('bold')}
+          onClick={onToggleBold}
+        >
+          <Icon name="bold" />
+        </Button>
+        <Button
+          color="translucent"
+          ariaLabel="Italic text"
+          className={getFormatButtonClassName('italic')}
+          onClick={onToggleItalic}
+        >
+          <Icon name="italic" />
+        </Button>
+        <Button
+          color="translucent"
+          ariaLabel="Underlined text"
+          className={getFormatButtonClassName('underline')}
+          onClick={onToggleUnderline}
+        >
+          <Icon name="underlined" />
+        </Button>
+        <Button
+          color="translucent"
+          ariaLabel="Strikethrough text"
+          className={getFormatButtonClassName('strikethrough')}
+          onClick={onToggleStrikethrough}
+        >
+          <Icon name="strikethrough" />
+        </Button>
+        <Button
+          color="translucent"
+          ariaLabel="Monospace text"
+          className={getFormatButtonClassName('monospace')}
+          onClick={handleMonospaceText}
+        >
+          <Icon name="monospace" />
+        </Button>
+        <Button
+          color="translucent"
+          ariaLabel="Quote text"
+          className={getFormatButtonClassName('quote')}
+          onClick={handleQuoteText}
+        >
+          <Icon name="quote" />
+        </Button>
+        <div className="TextFormatter-divider" />
+        <Button color="translucent" ariaLabel={lang('TextFormat.AddLinkTitle')} onClick={openLinkControl}>
+          <Icon name="link" />
+        </Button>
+      </div>
+
+      <div className="TextFormatter-link-control">
+        <div className="TextFormatter-buttons">
+          <Button color="translucent" ariaLabel={lang('Cancel')} onClick={closeLinkControl}>
+            <Icon name="arrow-left" />
+          </Button>
+          <div className="TextFormatter-divider" />
+
+          <div
+            className={buildClassName('TextFormatter-link-url-input-wrapper', inputClassName)}
+          >
+            <input
+              ref={linkUrlInputRef}
+              className="TextFormatter-link-url-input"
+              type="text"
+              value={linkUrl}
+              placeholder="Enter URL..."
+              autoComplete="off"
+              inputMode="url"
+              dir="auto"
+              onChange={handleLinkUrlChange}
+              onScroll={updateInputStyles}
+            />
+          </div>
+
+          <div className={linkUrlConfirmClassName}>
+            <div className="TextFormatter-divider" />
+            <Button
+              color="translucent"
+              ariaLabel={lang('Save')}
+              className="color-primary"
+              onClick={handleLinkUrlConfirm}
+            >
+              <Icon name="check" />
+            </Button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default memo(TextFormatter);
